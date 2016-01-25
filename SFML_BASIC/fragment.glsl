6235c5c7f9fe25bf131b46e6dc45184dcb6f26e8
Jason Power <c00185326@itcarlow.ie>
2016-01-25 15:22:29 +0000

    Multi Texture
    Needs a bit of love but we can sort it

--- SFML_BASIC/fragment.glsl
+++ SFML_BASIC/fragment.glsl
@@ -1,9 +1,23 @@
 uniform sampler2D sea;
 uniform sampler2D grass;
 uniform sampler2D rock;
- 
+
+varying float height;
+
 void main()
 {
-    vec4 color = texture2D(rock,gl_TexCoord[0].st);
-    gl_FragColor = color;
+	// Heights are between 0 - 18 I think ?
+	// Because we got the vertex height and not the pixel height; 
+
+   // vec4 color = texture2D(rock,gl_TexCoord[0].st);
+   // gl_FragColor = color;
+
+	if(height >= 0 && height <= 2)
+		gl_FragColor = texture2D(sea, gl_TexCoord[0].st );  
+	else if(height > 2 && height <= 8)
+		gl_FragColor = texture2D(grass, gl_TexCoord[0].st );  
+
+	else 
+		gl_FragColor = texture2D(rock, gl_TexCoord[0].st );  
+
 }
--- SFML_BASIC/vertex.glsl
+++ SFML_BASIC/vertex.glsl
@@ -1,6 +1,13 @@
+varying  float height;
+
 void main()
 {
+	// Get the height
+	height = gl_Vertex.y;
+
 	gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+	gl_TexCoord[1] = gl_TextureMatrix[1] * gl_MultiTexCoord1;
+	gl_TexCoord[2] = gl_TextureMatrix[2] * gl_MultiTexCoord2;
 	gl_FrontColor = gl_Color; //pass the colour along to the next pahse (fragment shader);
 	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
 }
